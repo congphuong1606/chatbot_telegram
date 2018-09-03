259773dac0cedd498cfd32bcf385547b2bc13da5
congphuong1606 <cphuong.coldboy@gmail.com>
2018-09-03 15:56:42 +0700

    Sfsdf

--- test.js
+++ test.js
@@ -1,15 +1,10 @@
-var moment = require('moment');
+var http=require('request');
 
 
+http("https://script.google.com:443/macros/s/AKfycbzh3oR1kj1MoieKw16Re4ee0TH76-khSMaovjOlSFrpUJtnp9k/exec?action=read-all-data&amp;alt=json", function (error, response, body) {
+        console.log('Response is '+response);
+		console.log('err is '+error);
+		console.log('data is '+body);
 
-var startTime=moment().valueOf();
-var endTime=0;
-setTimeout(function () {
-    var endTime=moment().valueOf();
-    var duration =parseInt((endTime-startTime)/1000);
-}, 10000);
-
-
-
-
-
+    });
+       
