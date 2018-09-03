dda0cf9b15cc63b426c67b42a330059108a1ff07
congphuong1606 <cphuong.coldboy@gmail.com>
2018-09-03 12:26:32 +0700

    sdsdS

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ wss.on("connection", function(ws) {
 const TelegramBot = require('node-telegram-bot-api');
 const token = '694156014:AAGSi9FtWPbHODSAowRylOPtHmLUPDSN2i4';
 const bot = new TelegramBot(token, {polling: true});
-var url="https://script.google.com/macros/s/AKfycbzh3oR1kj1MoieKw16Re4ee0TH76-khSMaovjOlSFrpUJtnp9k/exec?action=read-all-data&alt=json";
+var url="http://script.google.com/macros/s/AKfycbzh3oR1kj1MoieKw16Re4ee0TH76-khSMaovjOlSFrpUJtnp9k/exec?action=read-all-data&alt=json";
 var callapi = require('request');
 
 var moment = require('moment');
